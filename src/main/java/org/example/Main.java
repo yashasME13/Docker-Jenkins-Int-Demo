d39aa436f87d6571fc347faa5382abc0c2e3def0
Docker-Jenkins-Int-Demo <yashasme13@gmail.com>
2025-08-17 00:52:15 +0530

    forth push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome! to 5th build");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
